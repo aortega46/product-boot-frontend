83d0b2194427b85e33207a3218142a6d9c3da2de
Albert Ortega <trebla.og46@gmail.com>
2024-04-11 12:56:23 +0200

    feat: :sparkles: product item to detail routerlink

--- src/app/product-item/product-item.component.ts
+++ src/app/product-item/product-item.component.ts
@@ -1,12 +1,14 @@
 import { CurrencyPipe } from '@angular/common'
 import { Component, Input } from '@angular/core'
+import { RouterLink } from '@angular/router'
+import { Product } from '../interfaces/product'
 
 @Component({
   selector: 'app-product-item',
   standalone: true,
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, RouterLink],
   templateUrl: './product-item.component.html',
 })
 export class ProductItemComponent {
-  @Input() product?: any
+  @Input() product!: Product
 }
