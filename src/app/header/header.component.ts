17ced04e69e1003bf9f1f926c7188e59dc9b2406
Albert Ortega <trebla.og46@gmail.com>
2024-04-11 13:39:02 +0200

    feat: :sparkles: cart component, added to header too
    header styles

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
 import { RouterLink } from '@angular/router'
+import { CartComponent } from '../cart/cart.component'
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CartComponent],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {}
