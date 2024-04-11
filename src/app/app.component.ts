eb8501cf0f189b2a552109cd7c926ad237400226
Albert Ortega <trebla.og46@gmail.com>
2024-04-11 12:55:02 +0200

    feat: :lipstick: adjust height size to viewport height

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
-import { ProductsListComponent } from './products-list/products-list.component'
 import { HeaderComponent } from './header/header.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductsListComponent, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
