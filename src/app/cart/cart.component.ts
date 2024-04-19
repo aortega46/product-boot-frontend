9c217f3a8496b1f70919120678242e5119074140
Albert Ortega <trebla.og46@gmail.com>
2024-04-19 12:38:50 +0200

    feat: :sparkles: cart item template

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core'
+import { Component, inject } from '@angular/core'
+import { CartService } from '../services/cart/cart.service'
+import { AsyncPipe, CurrencyPipe, NgTemplateOutlet } from '@angular/common'
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, NgTemplateOutlet, CurrencyPipe],
   templateUrl: './cart.component.html',
 })
 export class CartComponent {
+  cartService = inject(CartService)
+
   isCartOpened: boolean = false
 
   toggleCart() {
     this.isCartOpened = !this.isCartOpened
   }
 
-  // TODO: clearCart
-  clearCart() {}
+  clearCart() {
+    this.cartService.clearCart()
+  }
 }
