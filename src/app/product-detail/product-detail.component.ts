35f4d5d94b9c5d0f81ae6ec0fc07b821dc026109
Albert Ortega <trebla.og46@gmail.com>
2024-04-19 12:42:43 +0200

    feat: :sparkles: product detail button adds/removes to cart

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit, inject } from '@angular/core'
 import { ActivatedRoute, RouterLink } from '@angular/router'
 import { Product } from '../interfaces/product'
 import { ProductsService } from '../services/products/products.service'
-import { CurrencyPipe } from '@angular/common'
+import { AsyncPipe, CurrencyPipe } from '@angular/common'
+import { CartService } from '../services/cart/cart.service'
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [CurrencyPipe, RouterLink],
+  imports: [CurrencyPipe, RouterLink, AsyncPipe],
   templateUrl: './product-detail.component.html',
   styles: `
     :host {
@@ -18,6 +19,7 @@ import { CurrencyPipe } from '@angular/common'
 export class ProductDetailComponent implements OnInit {
   private route = inject(ActivatedRoute)
   private productsService = inject(ProductsService)
+  cartService = inject(CartService)
 
   product?: Product
 
