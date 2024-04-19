b6e0090d1be0486d19edc5f1a9cd4e803eda1b3b
Albert Ortega <trebla.og46@gmail.com>
2024-04-19 12:36:30 +0200

    feat: :sparkles: product item button adds/removes to cart

--- src/app/product-item/product-item.component.ts
+++ src/app/product-item/product-item.component.ts
@@ -1,14 +1,17 @@
-import { CurrencyPipe } from '@angular/common'
-import { Component, Input } from '@angular/core'
+import { AsyncPipe, CurrencyPipe } from '@angular/common'
+import { Component, Input, inject } from '@angular/core'
 import { RouterLink } from '@angular/router'
 import { Product } from '../interfaces/product'
+import { CartService } from '../services/cart/cart.service'
 
 @Component({
   selector: 'app-product-item',
   standalone: true,
-  imports: [CurrencyPipe, RouterLink],
+  imports: [CurrencyPipe, RouterLink, AsyncPipe],
   templateUrl: './product-item.component.html',
 })
 export class ProductItemComponent {
+  cartService = inject(CartService)
+
   @Input() product!: Product
 }
