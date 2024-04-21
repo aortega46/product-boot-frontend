4f3c193631d6b9a60b1b743f80868594f01fde66
Albert Ortega <trebla.og46@gmail.com>
2024-04-21 15:49:49 +0200

    feat: :sparkles: Product service get product by name

--- src/app/services/products/products.service.ts
+++ src/app/services/products/products.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable, inject } from '@angular/core'
-import { Observable, tap } from 'rxjs'
+import { Observable, of } from 'rxjs'
+import { Product } from '../../interfaces/product'
 
 @Injectable({
   providedIn: 'root',
@@ -15,5 +16,13 @@ export class ProductsService {
 
   public getProductById(id: string): Observable<any> {
     return this.http.get(`${this.baseUrl}/api/products/${id}`)
+  }
+
+  public getProductByName(name: string): Observable<Product[]> {
+    if (!name || name.length < 3) return of([])
+
+    return this.http.get<Product[]>(
+      `${this.baseUrl}/api/products/search?name=${name}`
+    )
   }
 }
