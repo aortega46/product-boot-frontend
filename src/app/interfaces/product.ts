926526355905a1ef2a98985b0f529d4233dd7b40
Albert Ortega <trebla.og46@gmail.com>
2024-04-25 07:52:23 +0200

    refactor: :recycle: product interface

--- src/app/interfaces/product.ts
+++ src/app/interfaces/product.ts
@@ -1,12 +1,9 @@
-import { Image } from './image'
-import { Category } from './category'
-
 export interface Product {
   id: number
   name: string
   price: number
   description: string
   thumbnail: string
-  images: Image[]
-  categories: Category[]
+  images: string[]
+  categories: string[]
 }
